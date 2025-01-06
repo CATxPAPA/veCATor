2af4bd3b8891831fa385610b4898b78e5a60aba8
CATxPAPA <catxpapa@gmail.com>
2025-01-06 22:54:14 +0100

    重构应用程序结构，将依赖项移至前端，删除后端不再使用的 requirements.txt 文件，更新构建脚本和配置文件以适应新的目录结构

--- vecator/build.sh
+++ vecator/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # 删除旧的 dist 目录
-rm -rf ./dist
+#rm -rf ./dist
 
 # 创建 dist 目录
-mkdir -p dist
+#mkdir -p dist
 
 # 复制后端代码
-cp -r backend dist/
+#cp -r backend dist/
 
 # 复制前端资源
-mkdir -p dist/ui
-cp -r ui/static dist/ui
-cp -r ui/templates dist/ui
+#mkdir -p dist/ui
+#cp -r ui/static dist/ui
+#cp -r ui/templates dist/ui
+echo 'Building'
